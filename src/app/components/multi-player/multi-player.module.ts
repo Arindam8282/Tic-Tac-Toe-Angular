0a277aaa644dad404051715e7724d3d62c2e21c6
Arindam8282 <darindam507@gmail.com>
2021-01-23 21:14:42 +0530

    prime ng module added

---
+++ src/app/components/multi-player/multi-player.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MultiPlayerComponent } from './multi-player.component';
+
+
+
+@NgModule({
+  declarations: [MultiPlayerComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class MultiPlayerModule { }
---
+++ src/app/components/single-player/single-player.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SinglePlayerComponent } from './single-player.component';
+
+
+
+@NgModule({
+  declarations: [SinglePlayerComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SinglePlayerModule { }
