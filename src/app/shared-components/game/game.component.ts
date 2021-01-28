1cf1d8acafbf172c6d7d173afe3e1e7eb80a213f
Arindam8282 <darindam507@gmail.com>
2021-01-28 19:20:15 +0530

    single player completed

--- src/app/shared-components/game/game.component.ts
+++ src/app/shared-components/game/game.component.ts
@@ -12,7 +12,8 @@ import {Message} from 'primeng/api';
 })
 export class GameComponent implements OnInit {
   msgs: Message[] = [];
-
+  gameover: boolean = false;
+  cursorClass: String = "";
   constructor(private primeNg: PrimeNGConfig,
               private confirmationService: ConfirmationService,
               public gameData: GameDataService,
@@ -23,6 +24,10 @@ export class GameComponent implements OnInit {
   ngOnInit(): void {
     this.primeNg.ripple = true;
   }
+  ability(item) {
+    if(item!='') return "dis-cursor";
+    else return "";
+  }
   reset(){
     this.gameData.arr = ['','','','','','','','',''];
     this.gameData.opoScore = this.gameData.tied = this.gameData.yourScore = 0;
@@ -32,6 +37,7 @@ export class GameComponent implements OnInit {
         message: 'Do you want to exit?',
         header: 'Confirmation',
         icon: 'pi pi-exclamation-triangle',
+        defaultFocus:'none',
         accept: () => {
             this.reset();
             this.route.navigate(['/']);
@@ -42,39 +48,120 @@ export class GameComponent implements OnInit {
         }
     });
   }
+ 
+  insert(index,icon){
+    let i=0;
+    if(this.gameData.arr[index]=='') 
+      this.gameData.arr[index] = icon;
+    
+    else return false;
+    this.cursorClass = "dis-cursor";
+    if(this.isGameover()) return true;
+    this.botPlayer();
+  }
   getRand() {
     return Math.floor((Math.random() * 9) + 0);
   }
-  insert(index){
-    let i=0;
-    if(this.gameData.arr[index]=='') {
-      this.gameData.arr[index] = 'pi-check';
-      this.gameData.yourScore+=1;
-    }
-    else return false;
-    while(this.gameData.arr[i]!='' && i<8) i++;
-    if(i==8) return false; 
+  botPlayer() {
     this.gameData.blocked = true;
     this.gameData.playerTurn = "Bot's";
-    this.easyMode();
-  }
-  easyMode() {
     let num =0;
     setTimeout(() => {
       this.gameData.playerTurn = "Your";
       num = this.getRand();
       while(this.gameData.arr[num]!='') num = this.getRand();
-      this.gameData.arr[num] = 'pi-times';
-    this.gameData.blocked = false;
+      this.gameData.arr[num] = this.gameData.player2Icon;
+      this.gameData.blocked = false;
+    if(this.isGameover()) return true;
     }, 500);
   }
-  mediumMode() {
-    let num =0;
-    setTimeout(() => {
-      num = this.getRand();
-      while(this.gameData.arr[num]!='') num = this.getRand();
-      this.gameData.arr[num] = 'pi-times';
-    this.gameData.blocked = false;
-    }, 1000);
+
+  isGameover() {
+    let checker = this.check();
+    let winner = '';
+    if(checker==1 || checker==0 || checker<0) {
+      if(checker == 1) {
+        this.gameData.yourScore+=1;
+        winner = "You've won";
+      }
+      if(checker == 0) {
+        this.gameData.opoScore+=1;
+        winner = "Bot won";
+      }
+      if(checker<0) {
+        this.gameData.tied+=1;
+        winner = "Game Tied";
+      }
+      this.gameData.winner = winner;
+      this.gameData.arr = ['','','','','','','','',''];
+      this.gameover = true;
+      let counter = 3;
+      var myVar;
+      var time = 1000;
+      myVar = setInterval(() => {
+        this.gameover = false;
+        this.confirmationService.confirm({
+          message: winner,
+          header: 'Play Again',
+          acceptLabel: 'Exit',
+          rejectLabel:  'press or replaying..'+counter,
+          rejectIcon: 'pi pi-undo',
+          acceptIcon: 'pi',
+          acceptButtonStyleClass:'p-button-danger',
+          rejectButtonStyleClass:'p-button-secondary',
+          defaultFocus:'none',
+          accept: () => {
+            this.reset();
+            clearInterval(myVar);
+            this.route.navigate(['/']);
+              // this.msgs = [{severity:'info', summary:'Confirmed', detail:'You have accepted'}];
+          },
+          reject: () => {
+            this.confirmationService.close();
+            clearInterval(myVar);
+              // this.msgs = [{severity:'info', summary:'Rejected', detail:'You have rejected'}];
+          }
+        });
+       
+          if(counter<1) {
+            this.confirmationService.close();
+            clearInterval(myVar);
+          } 
+          counter-=1;
+      }, time);
+      return true;
+    }
+    else return false;
   }
+  check() {
+    if((this.gameData.arr[0]=='pi-times') && (this.gameData.arr[1]=='pi-times') && (this.gameData.arr[2]=='pi-times')) return 0;
+    if((this.gameData.arr[0]=='pi-check') && (this.gameData.arr[1]=='pi-check') && (this.gameData.arr[2]=='pi-check')) return 1;
+    
+    if((this.gameData.arr[3]=='pi-times') && (this.gameData.arr[4]=='pi-times') && (this.gameData.arr[5]=='pi-times')) return 0;  
+    if((this.gameData.arr[3]=='pi-check') && (this.gameData.arr[4]=='pi-check') && (this.gameData.arr[5]=='pi-check')) return 1;
+
+    if ((this.gameData.arr[6]=='pi-times') && (this.gameData.arr[7]=='pi-times') && (this.gameData.arr[8]=='pi-times')) return 0;
+    if((this.gameData.arr[6]=='pi-check') && (this.gameData.arr[7]=='pi-check') && (this.gameData.arr[8]=='pi-check')) return 1;
+
+    if((this.gameData.arr[0]=='pi-times') && (this.gameData.arr[3]=='pi-times') && (this.gameData.arr[6]=='pi-times')) return 0;
+    if((this.gameData.arr[0]=='pi-check') && (this.gameData.arr[3]=='pi-check') && (this.gameData.arr[6]=='pi-check')) return 1;
+
+    if ((this.gameData.arr[1]=='pi-times') && (this.gameData.arr[4]=='pi-times') && (this.gameData.arr[7]=='pi-times')) return 0;
+    if((this.gameData.arr[1]=='pi-check') && (this.gameData.arr[4]=='pi-check') && (this.gameData.arr[7]=='pi-check'))  return 1;
+
+    if((this.gameData.arr[2]=='pi-times') && (this.gameData.arr[5]=='pi-times') && (this.gameData.arr[8]=='pi-times')) return 0;
+    if((this.gameData.arr[2]=='pi-check') && (this.gameData.arr[5]=='pi-check') && (this.gameData.arr[8]=='pi-check')) return 1;
+
+    if((this.gameData.arr[2]=='pi-times') && (this.gameData.arr[4]=='pi-times') && (this.gameData.arr[6]=='pi-times')) return 0;
+    if((this.gameData.arr[2]=='pi-check') && (this.gameData.arr[4]=='pi-check') && (this.gameData.arr[6]=='pi-check')) return 1;
+
+    if((this.gameData.arr[0]=='pi-times') && (this.gameData.arr[4]=='pi-times') && (this.gameData.arr[8]=='pi-times')) return 0;
+    if((this.gameData.arr[0]=='pi-check') && (this.gameData.arr[4]=='pi-check') && (this.gameData.arr[8]=='pi-check')) return 1;
+
+    if((this.gameData.arr[0]== 'pi-times'|| this.gameData.arr[0]=='pi-check') && (this.gameData.arr[1]=='pi-times' || this.gameData.arr[1]=='pi-check') && (this.gameData.arr[2]=='pi-times' || this.gameData.arr[2]=='pi-check') && (this.gameData.arr[3]=='pi-times' || this.gameData.arr[3]=='pi-check') && (this.gameData.arr[4]=='pi-times' || this.gameData.arr[4]=='pi-check') && (this.gameData.arr[5]=='pi-times' || this.gameData.arr[5]=='pi-check') && (this.gameData.arr[6]=='pi-times' || this.gameData.arr[6]=='pi-check') && (this.gameData.arr[7]=='pi-times' || this.gameData.arr[7]=='pi-check') && (this.gameData.arr[8]=='pi-times' || this.gameData.arr[8]=='pi-check'))
+      return -1;
+
+    return 2;
+  }
+ 
 }
--- src/app/shared-components/gameData/game-data.service.ts
+++ src/app/shared-components/gameData/game-data.service.ts
@@ -5,9 +5,14 @@ import { Injectable } from '@angular/core';
 })
 export class GameDataService {
   display: boolean = true;
+  winner: String = '';
   arr: String[]=['','','','','','','','',''];
   blocked: boolean = false;
   playerTurn: String = "Your";
+  player1Name: String  = "Player1";
+  player2Name: String  = "Player2";
+  player1Icon: String = "pi-check";
+  player2Icon: String = "pi-times";
   yourScore: number = 0;
   opoScore: number = 0;
   tied: number = 0;
